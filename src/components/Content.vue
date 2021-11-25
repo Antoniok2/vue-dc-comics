6d9d25288bf8563f213e39191ce5cbcabfb6bb9b
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-25 16:07:54 +0100

    schedule product complete

---
+++ src/components/Content.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="scheda">
+      <img :src="details.thumb" :alt="details.type">
+      <span>{{details.series}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  props: {
+    details: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .scheda {
+        width: calc((100% / 6) - 56px);
+        height: 40%;
+        margin: 28px;
+
+        img {
+            width: 80%;
+            height: 80%;
+        }
+
+        span {
+            color: white;
+        }
+    }
+</style>
